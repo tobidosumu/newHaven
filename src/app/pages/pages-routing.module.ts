542912cd54305f93c37c47496151ab1900628b2d
tobiDosumu <dosunmumishael@gmail.com>
2022-08-02 18:34:58 +0100

    fixed repo issue

---
+++ src/app/pages/pages-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PagesComponent } from './pages.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { DonationComponent } from './donation/donation.component';
+
+const routes: Routes = [
+  { path: '', 
+    component: PagesComponent,
+    children: [
+      { path: 'home', component: HomeComponent },
+      { path: 'profile', component: ProfileComponent },
+      { path: 'donation', component: DonationComponent}
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class PagesRoutingModule { }
---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from './../shared/shared.module';
+import { PagesRoutingModule } from './pages-routing.module';
+
+import { PagesComponent } from './pages.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { DonationComponent } from './donation/donation.component';
+import { CongratsComponent } from './congrats/congrats.component';
+import { FaqComponent } from './faq/faq.component';
+import { CauseCardComponent } from './home/cause-card/cause-card.component';
+
+
+
+@NgModule({
+  declarations: [
+    PagesComponent,
+    HomeComponent,
+    ProfileComponent,
+    DonationComponent,
+    CongratsComponent,
+    FaqComponent,
+    CauseCardComponent
+  ],
+  imports: [
+    CommonModule,
+    PagesRoutingModule,
+    SharedModule
+  ]
+})
+export class PagesModule { }
