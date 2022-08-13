b9bf05e40b8ecfef3d0366fb650277ba525e5b92
tobiDosumu <dosunmumishael@gmail.com>
2022-08-13 19:24:14 +0100

    remove commented out code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
-// import { PagesRoutingModule } from './pages/pages-routing.module';
-// import { PagesModule } from './pages/pages.module';
-
 
 import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-// import { ProfileComponent } from './pages/profile/profile.component'
-
 
 @NgModule({
   declarations: [
@@ -21,7 +16,6 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
     BrowserModule,
     AppRoutingModule,
     SharedModule,
-    // PagesRoutingModule
 
   ],
   providers: [],
