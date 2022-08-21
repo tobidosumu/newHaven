332d055b4f1afa3a5fbf9db038ce176c7f9161f9
Yeshuaobi <joshobi984@gmail.com>
2022-08-21 23:20:35 +0100

    page not found almost done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
@@ -7,6 +8,7 @@ const routes: Routes = [
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
   { path: '', loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule) },
   { path: 'shared', loadChildren: () => import('./shared/shared.module').then(m => m.SharedModule) },
+  { path: '**', component: PageNotFoundComponent}
 ];
 
 @NgModule({
