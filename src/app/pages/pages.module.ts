ae2a70367d19d0351998b1a98b715dfb089f3241
tobiDosumu <dosunmumishael@gmail.com>
2022-08-30 20:50:27 +0100

    fixing bug preventing angular from displaying in browser

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,16 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from './../shared/shared.module';
 import { PagesRoutingModule } from './pages-routing.module';
-import { FormsModule } from '@angular/forms';
-import { MaterialModule } from '../material/material.module';
+import { MaterialModule } from '../material/material.module'
 
 import { PagesComponent } from './pages.component';
 import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
 import { DonationComponent } from './donation/donation.component';
 import { FaqComponent } from './faq/faq.component';
-import { UserProfileComponent } from './user-profile/user-profile.component';
+// import { CauseCardComponent } from './home/cause-card/cause-card.component';
 import { CharitiesComponent } from './home/charities/charities.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+
 
 @NgModule({
   declarations: [
@@ -20,14 +21,14 @@ import { CharitiesComponent } from './home/charities/charities.component';
     ProfileComponent,
     DonationComponent,
     FaqComponent,
-    UserProfileComponent,
+    // CauseCardComponent,
     CharitiesComponent,
+    UserProfileComponent,
   ],
   imports: [
     CommonModule,
     PagesRoutingModule,
     SharedModule,
-    FormsModule,
     MaterialModule
   ]
 })
