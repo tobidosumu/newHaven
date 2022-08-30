e9c99efdc569091c2099320b0cab924009406eb8
tobiDosumu <dosunmumishael@gmail.com>
2022-08-30 13:32:13 +0100

    fix bugs causing 'cannot get error' by: - remove material angular - update @angular/common and some other dependencies

---
+++ newHaven-main/src/app/pages/home/cause-card/cause-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cause-card',
+  templateUrl: './cause-card.component.html',
+  styleUrls: ['./cause-card.component.scss']
+})
+export class CauseCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
